9a4d6cee5e37345410742e69a78fcaedbde67b36
Victor <v.potenciano@bi4group.com>
2021-10-19 18:29:59 +0200

    second commit

---
+++ src/main/java/com/example/thegarage/exception/NoSuchElementFoundException.java
@@ -0,0 +1,7 @@
+package com.example.thegarage.exception;
+
+public class NoSuchElementFoundException extends RuntimeException {
+    public NoSuchElementFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/example/thegarage/service/GarageService.java
+++ src/main/java/com/example/thegarage/service/GarageService.java
@@ -10,6 +10,7 @@ import com.example.thegarage.model.Vehicle;
 import com.example.thegarage.repository.GarageRepository;
 import com.example.thegarage.repository.VehicleRepository;
 import com.example.thegarage.model.Location;
+import com.example.thegarage.exception.NoSuchElementFoundException;
 
 
 @Service
@@ -20,33 +21,35 @@ public class GarageService {
     @Autowired
     private VehicleRepository vehicles;
 
-    public Location enter(Long garageId, String vehicleId) {
-        Garage garage = this.garages.findById(garageId);
-        Vehicle vehicle = this.vehicles.findById(vehicleId);
-
+    public Garage getGarage(Long id) {
+        Garage garage = this.garages.findById(id);
         if (garage == null) {
-            // TODO raise not found
+            throw new NoSuchElementFoundException("Garage with id " + id + " not found");
         }
-        if (vehicle == null) {
-            // TODO raise not found
-        }
+        return garage;
+    }
 
+    public Vehicle getVehicle(String id) {
+        Vehicle vehicle = this.vehicles.findById(id);
+        if (vehicle == null) {
+            throw new NoSuchElementFoundException("Vehicle with id " + id + " not found");
+        }
+        return vehicle;
+    }
+
+    public Location enter(Long garageId, String vehicleId) {
+        Garage garage = this.getGarage(garageId);
+        Vehicle vehicle = this.getVehicle(vehicleId);
         return garage.assign(vehicle);
     }
 
     public void exit(String vehicleId) {
-        Vehicle vehicle = this.vehicles.findById(vehicleId);
-        if (vehicle == null) {
-            // TODO raise not found
-        }
+        Vehicle vehicle = this.getVehicle(vehicleId);
         vehicle.exit();
     }
 
     public Location locate(String vehicleId) {
-        Vehicle vehicle = this.vehicles.findById(vehicleId);
-        if (vehicle == null) {
-            // TODO raise not found
-        }
+        Vehicle vehicle = this.getVehicle(vehicleId);
         return vehicle.locate();
     }
 }
